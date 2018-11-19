bc1f53166458e55a2e2bca2712458b4c4212c6db
roarra <43341488+roarra@users.noreply.github.com>
2018-11-19 11:37:21 +0100

    PRUEBAS UNITARIAS PELICULA CON JAVADOC

--- src/test/TestPelicula.java
+++ src/test/TestPelicula.java
@@ -2,16 +2,20 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
-import java.io.IOException;
-
-import org.junit.Test;
-
-import datos.PeliculasImp;
-import modelo.Pelicula;
+/**
+ * 
+ * @author Maria Victoria
+ * 
+ * Definimos esta clase que contiene los metodos que realizan las pruebas unitarias
+ *
+ */
 
 public class TestPelicula {
 
-		
+	/**
+	 * 
+	 * En este metodo definimos que la pelicula está ya en la BBDD
+	 */
 	//PARA COMPROBAR QUE LA PELICULA ESTÁ REPETIDA
 	
 	@org.junit.Test
@@ -23,15 +27,11 @@ public class TestPelicula {
 		
 		}
 	
-	//PARA COMPROBAR QUE LA PELICULA SE ENCUENTRA EN LA BBDD
+	/**
+	 * 
+	 * En este metodo definimos que la pelicula no se encuentra en la BBDD
+	 */
 	
-	@org.junit.Test
-	public void CheckNullFilm(){
-		String resultadoReal = ("SELECT * FROM peliculas WHERE nombre= 'Avatar'");
-		String resultadoEsperado = ("La pelicula se encuentra en la BBDD");
-		assertNotNull(resultadoEsperado,resultadoReal);
-		
-		}
 	
 	//COMPROBAMOS QUE LA PELICULA NO SE ENCUENTRA EN LA BBD
 	
