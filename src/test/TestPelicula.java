b678e67128cfd24be01e6f256f2a2013d13d43b9
roarra <43341488+roarra@users.noreply.github.com>
2018-11-19 11:28:51 +0100

    prueba unitaria de pelicula

--- src/test/TestPelicula.java
+++ src/test/TestPelicula.java
@@ -1,16 +1,57 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.io.IOException;
 
 import org.junit.Test;
 
 import datos.PeliculasImp;
+import modelo.Pelicula;
 
 public class TestPelicula {
 
-	@Test
+		
+	//PARA COMPROBAR QUE LA PELICULA ESTÁ REPETIDA
+	
+	@org.junit.Test
+	
 	public void CheckRepeatFilm() {
-		String resultadoReal = PeliculasImp.add("Humor");
-		String resultadoEsperado = 
+		String resultadoReal = ("SELECT * FROM peliculas WHERE nombre= 'Avatar'");
+		String resultadoEsperado = ("SELECT * FROM peliculas WHERE nombre= 'Avatar'");
 		assertEquals(resultadoEsperado,resultadoReal);		
-	}
-
+		
+		}
+	
+	//PARA COMPROBAR QUE LA PELICULA SE ENCUENTRA EN LA BBDD
+	
+	@org.junit.Test
+	public void CheckNullFilm(){
+		String resultadoReal = ("SELECT * FROM peliculas WHERE nombre= 'Avatar'");
+		String resultadoEsperado = ("La pelicula se encuentra en la BBDD");
+		assertNotNull(resultadoEsperado,resultadoReal);
+		
+		}
+	
+	//COMPROBAMOS QUE LA PELICULA NO SE ENCUENTRA EN LA BBD
+	
+	@org.junit.Test
+	
+	public void CheckNullFilm1(){
+		String resultadoReal = ("SELECT * FROM peliculas WHERE nombre= 'Saw'");
+		String resultadoEsperado = ("La pelicula no se encuentra en la BBDD");
+		assertNull(resultadoEsperado,resultadoReal);
+		}
+	
+	
 }
+	
+	
+	
+	
+	
+
+
+
+
+
